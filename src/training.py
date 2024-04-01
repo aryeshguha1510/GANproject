99c30d766926c25934cd20c60024943a168552a1
Aryesh Guha <149876009+aryeshguha1510@users.noreply.github.com>
2024-04-01 20:28:00 +0530

    indentation fixed

--- src/training.py
+++ src/training.py
@@ -31,49 +31,50 @@ def train_model(model, train_dataloader, val_dataloader, optimizer, num_epochs):
             images = batch['image']
             labels = batch['target']
             outputs = model(images)
-        loss = F.l1_loss(outputs, labels)
+            loss = F.l1_loss(outputs, labels)
             
             # clear gradients for this training step
-        optimizer.zero_grad()
+            optimizer.zero_grad()
             
             # backpropagation, compute gradients
-        loss.backward()
+            loss.backward()
             
             # apply gradients
-        optimizer.step()
+            optimizer.step()
             
-        for i in range(len(outputs)):
-            train_psnr_total += psnr(labels[i].numpy(), outputs[i].detach().numpy())
+            for i in range(len(outputs)):
+                train_psnr_total += psnr(labels[i].numpy(), outputs[i].detach().numpy())
            # train_ssim_total += ssim(labels[i].numpy(), outputs[i].detach().numpy(),wiz_size=11, channel_axis=3, multichannel=True)
 
-    train_psnr_avg = train_psnr_total / len(loaders['train'])
-    #train_ssim_avg = train_ssim_total / len(loaders['train'])
+        train_psnr_avg = train_psnr_total / len(loaders['train'])
+        #train_ssim_avg = train_ssim_total / len(loaders['train'])
         
-    model.eval()
-    val_psnr_total = 0.0
-    #val_ssim_total = 0.0
-    with torch.no_grad():
-        for batch in loaders['val']:
-            images = batch['image']
-            labels = batch['target']
+        model.eval()
+        val_psnr_total = 0.0
+        #val_ssim_total = 0.0
+        best_val_psnr_avg=0.0
+        with torch.no_grad():
+            for batch in loaders['val']:
+                images = batch['image']
+                labels = batch['target']
             
-            # Forward pass
-            outputs = model(images)
+                # Forward pass
+                outputs = model(images)
             
-            # Calculate PSNR and SSIM for validation set
-            for i in range(len(outputs)):
-                val_psnr_total += psnr(labels[i].numpy(), outputs[i].numpy())
-               # val_ssim_total += ssim(labels[i].numpy(), outputs[i].numpy(), multichannel=True)
+                # Calculate PSNR and SSIM for validation set
+                for i in range(len(outputs)):
+                    val_psnr_total += psnr(labels[i].numpy(), outputs[i].numpy())
+                  # val_ssim_total += ssim(labels[i].numpy(), outputs[i].numpy(), multichannel=True)
 
-    val_psnr_avg = val_psnr_total / len(loaders['val'])
-   # val_ssim_avg = val_ssim_total / len(loaders['val'])
+        val_psnr_avg = val_psnr_total / len(loaders['val'])
+      # val_ssim_avg = val_ssim_total / len(loaders['val'])
         # Print training and validation PSNR and SSIM
-    print(f'Epoch [{epoch+1}/{num_epochs}], Train PSNR: {train_psnr_avg:.2f}, Val PSNR: {val_psnr_avg:.2f}')
-    if (val_psnr_avg > best_val_psnr_avg):
-        best_val_psnr_avg = val_psnr_avg
-        #best_val_ssim_avg = val_ssim_avg
-        torch.save(model.state_dict(), 'weights.pth')
-        print('Weights Saved')
+        print(f'Epoch [{epoch+1}/{num_epochs}], Train PSNR: {train_psnr_avg:.2f}, Val PSNR: {val_psnr_avg:.2f}')
+        if (val_psnr_avg > best_val_psnr_avg):
+            best_val_psnr_avg = val_psnr_avg
+           #best_val_ssim_avg = val_ssim_avg
+            torch.save(model.state_dict(), 'weights.pth')
+            print('Weights Saved')
         
         
 if __name__ == '__main__':
